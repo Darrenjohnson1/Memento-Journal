7e36c9bd5c3dc6a85802cdedda7981aa77d2478d
Darrenjohnson1 <97546447+Darrenjohnson1@users.noreply.github.com>
2025-05-21 09:11:22 -0400

    Added Button Components

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,45 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Button } from "./ui/button";
+import LogoutButton from "./LogoutButton";
 
 function Header() {
+  const user = 1;
+
   return (
-    <header className='relative flex h-24 w-full items-center justify-between bg-popover px-3 sm:px-8'>
-        <Link href="/" className='flex items-end gap-2'>
-            <Image src="/starburst.png" height={60} width={60} alt="logo" className='rounded-full' priority/>
-            <h1>Memento Journal</h1>
-        </Link>
-        
+    <header className="bg-popover relative flex h-24 w-full items-center justify-between px-3 sm:px-8">
+      <Link href="/" className="flex items-end gap-2">
+        <Image
+          src="/starburst.png"
+          height={60}
+          width={60}
+          alt="logo"
+          className="rounded-full"
+          priority
+        />
+        <h1 className="flex flex-col pb-1 text-2xl leading-6 font-bold lg:text-3xl">
+          Memento Journal
+        </h1>
+      </Link>
+      <div className="flex gap-4">
+        {user ? (
+          <LogoutButton />
+        ) : (
+          <>
+            <Button asChild>
+              <Link href="/sign-up" className="hidden sm:block">
+                Sign Up
+              </Link>
+            </Button>
+            <Button asChild variant="outline">
+              <Link href="/login">Login</Link>
+            </Button>
+          </>
+        )}
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
---
+++ src/components/LogoutButton.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useState } from "react";
+import { Button } from "./ui/button";
+import { Loader2 } from "lucide-react";
+
+function LogoutButton() {
+  const [loading, setLoading] = useState(true);
+  return (
+    <Button>
+      {loading ? <Loader2 className="animate-spin" /> : "Log Out"}
+    </Button>
+  );
+}
+
+export default LogoutButton;
